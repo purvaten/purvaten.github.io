ff5f38c03182df6a3ac8cf4686b2201de9d40db6
Purva Tendulkar <purva.tendulkar@gmail.com>
2019-02-13 12:04:14 -0500

    variation of probabilistic comparison added

--- crowd-results/make_website.py
+++ crowd-results/make_website.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-base = "https://purvaten.github.io/"
-folders = ["crowd-results-original", "crowd-results-original-replace1", "crowd-results-original-replace2", "crowd-results-original-replace3"]
+base = "https://purvaten.github.io/crowd-results/"
+folders = ["crowd-results-original", "crowd-results-original-replace1", "crowd-results-original-replace2", "crowd-results-original-replace3", "crowd-results-original-replace4"]
 
 # get folder and filenames
 themes = []
 words = []
-urls = [[], [], [], []]
+urls = [[], [], [], [], []]
 
 for f, folder in enumerate(folders):
     for theme in os.listdir(folder):
@@ -20,13 +20,14 @@ for f, folder in enumerate(folders):
             themes.append(semicolon)
             words.append(img.split('.')[0])
 
-html_code = "<center><table border=1><tr><b><td>Word</td><td>Theme</td><td>Doodle1</td><td>Doodle2</td><td>Doodle3</td><td>Doodle4</td></b></tr>"
+html_code = "<center><table border=1><tr><b><td>Word</td><td>Theme</td><td>Doodle1</td><td>Doodle2</td><td>Doodle3</td><td>Doodle4</td><td>Doodle5</td></b></tr>"
 
 for i in range(104):
-    row_data = "<td>%s</td><td>%s</td><td><img src='%s'></td><td><img src='%s'></td><td><img src='%s'></td><td><img src='%s'></td>" % (words[i], themes[i], urls[0][i], urls[1][i], urls[2][i], urls[3][i])
+    row_data = "<td>%s</td><td>%s</td><td><img src='%s'></td><td><img src='%s'></td><td><img src='%s'></td><td><img src='%s'></td><td><img src='%s'></td>" % (words[i], themes[i], urls[0][i], urls[1][i], urls[2][i], urls[3][i], urls[4][i])
     html_code += "<tr>%s</tr>" % row_data
 
 html_code += "</table></center>"
 
 with open('index.html', 'w') as f:
     f.write(html_code)
+
