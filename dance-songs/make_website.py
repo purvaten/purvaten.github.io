d61167fd04c8f97b6f14a848ba01bfb5904702e7
Purva Tendulkar <purva.tendulkar@gmail.com>
2020-04-08 15:17:03 -0400

    jazz, rocknroll, instrumental songs added

--- dance-songs/make_website.py
+++ dance-songs/make_website.py
@@ -15,14 +15,16 @@ import os
 import pdb
 
 
-folders = ["acapella", "african", "american-pop", "bollywood", "chinese", "indian-classical", "latin", "no-lyrics", "offbeat", "rap"]
+folders = ["acapella", "african", "american-pop", "bollywood", "chinese", "indian-classical", "latin", "no-lyrics", "offbeat", "rap", "rocknroll", "jazz", "instrumental"]
 
 # get folder and filenames
+srno = []
 genres = []
 songnames = []
 songs = []
 durations = []
 music_matrices = []
+idx = 0
 
 for genre in folders:
     for songname in os.listdir(genre):
@@ -30,6 +32,10 @@ for genre in folders:
         # check that songname ends in .mp3
         if songname[-4:] != '.mp3':
             continue
+
+        # append srno
+        idx += 1
+        srno.append(idx)
 
         # append genre
         genres.append(genre)
@@ -46,29 +52,30 @@ for genre in folders:
         songs.append(genre + "/" + songname)
 
         # compute music matrix
-        hop_length = 128
-        lifter = 0
-        n_mfcc = 20
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc, lifter=lifter, hop_length=hop_length)
-        R = np.transpose(librosa.segment.recurrence_matrix(mfcc, sym=True))    # already normalized in 0-1
-        R = R + np.zeros(R.shape)    # change (True, False) to (1, 0)
-        np.fill_diagonal(R, 1)
-        if int(duration) != round(duration):
-            d = round(duration)
-        else:
-            d = int(duration)
-        plt.xticks(np.arange(0, R.shape[0], R.shape[0] / duration), np.arange(0, d, 1.0))
-        plt.yticks(np.arange(0, R.shape[0], R.shape[0] / duration), np.arange(0, d, 1.0))
-        plt.imshow(R)
-        plt.savefig(genre + "/" + songname + '.png')
+        # if genre in ["rocknroll", "jazz", "instrumental"]:
+        #     hop_length = 128
+        #     lifter = 0
+        #     n_mfcc = 20
+        #     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc, lifter=lifter, hop_length=hop_length)
+        #     R = np.transpose(librosa.segment.recurrence_matrix(mfcc, sym=True))    # already normalized in 0-1
+        #     R = R + np.zeros(R.shape)    # change (True, False) to (1, 0)
+        #     np.fill_diagonal(R, 1)
+        #     if int(duration) != round(duration):
+        #         d = round(duration)
+        #     else:
+        #         d = int(duration)
+        #     plt.xticks(np.arange(0, R.shape[0], R.shape[0] / duration), np.arange(0, d, 1.0))
+        #     plt.yticks(np.arange(0, R.shape[0], R.shape[0] / duration), np.arange(0, d, 1.0))
+        #     plt.imshow(R)
+        #     plt.savefig(genre + "/" + songname + '.png')
 
         # append music matrix
         music_matrices.append(genre + "/" + songname + '.png')
 
-html_code = "<center><table border=1><tr><b><td>Genre</td><td>Song Name</td><td>Audio</td><td>Duration (secs)</td><td>Music Matrix</td></b></tr>"
+html_code = "<center><table border=1><tr><b><td>Sr. No.</td><td>Genre</td><td>Song Name</td><td>Audio</td><td>Duration (secs)</td><td>Music Matrix</td></b></tr>"
 
 for i in range(len(songs)):
-    row_data = "<td>%s</td><td>%s</td><td><audio controls><source src=%s type='audio/mpeg'></audio></td><td>%s</td><td><img src=%s></td>" % (genres[i], songnames[i], songs[i], durations[i], music_matrices[i])
+    row_data = "<td>%d.</td><td>%s</td><td>%s</td><td><audio controls><source src=%s type='audio/mpeg'></audio></td><td>%s</td><td><img src=%s></td>" % (srno[i], genres[i], songnames[i], songs[i], durations[i], music_matrices[i])
     html_code += "<tr>%s</tr>" % row_data
 
 html_code += "</table></center>"
