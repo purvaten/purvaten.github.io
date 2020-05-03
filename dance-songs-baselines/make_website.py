3ad78cb375b8c7cb34549be2730bb4fea01aec75
Purva Tendulkar <purva.tendulkar@gmail.com>
2020-05-03 14:44:44 -0400

    baselines -- 4 random seeds

--- dance-songs-baselines/make_website.py
+++ dance-songs-baselines/make_website.py
@@ -9,7 +9,6 @@ python3.7 make_website
 """
 import numpy as np
 import librosa
-import madmom
 import pdb
 import os
 
@@ -23,10 +22,9 @@ songnames = []
 songs = []
 durations = []
 numbeats = []
-correlations = []
 music_matrix_upsize, music_matrix_downsize = [], []
 dance_matrix1, dance_matrix2, dance_matrix3, dance_matrix4 = [], [], [], []
-dance_video1, dance_video2, dance_video3, dance_video4 = [], [], [], []
+dance_video10, dance_video11, dance_video12, dance_video13, dance_video2, dance_video3, dance_video40, dance_video41, dance_video42, dance_video43 = [], [], [], [], [], [], [], [], [], []
 idx = 0
 
 for genre in folders:
@@ -58,19 +56,19 @@ for genre in folders:
         dance_matrix4.append(genre + "/" + currsong + "/dance_matrix4.png")
 
         # append dance videos location
-        dance_video1.append(genre + "/" + currsong + "/output1.mp4")
+        dance_video10.append(genre + "/" + currsong + "/output10.mp4")
+        dance_video11.append(genre + "/" + currsong + "/output11.mp4")
+        dance_video12.append(genre + "/" + currsong + "/output12.mp4")
+        dance_video13.append(genre + "/" + currsong + "/output13.mp4")
         dance_video2.append(genre + "/" + currsong + "/output2.mp4")
         dance_video3.append(genre + "/" + currsong + "/output3.mp4")
-        dance_video4.append(genre + "/" + currsong + "/output4.mp4")
+        dance_video40.append(genre + "/" + currsong + "/output40.mp4")
+        dance_video41.append(genre + "/" + currsong + "/output41.mp4")
+        dance_video42.append(genre + "/" + currsong + "/output42.mp4")
+        dance_video43.append(genre + "/" + currsong + "/output43.mp4")
 
-        # read the correlation information
-        correlations.append(np.loadtxt(genre + "/" + currsong + "/correlations.txt", dtype=float))
-
-        # get number of beats in song
-        proc = madmom.features.beats.DBNBeatTrackingProcessor(fps=100)
-        act = madmom.features.beats.RNNBeatProcessor()(genre + "/" + currsong + "/song.mp3")
-        beat_times = proc(act)
-        numbeats.append(len(beat_times))
+        # read the correlation information and number of beats in song
+        numbeats.append(np.loadtxt(genre + "/" + currsong + "/num_beats.txt", dtype=float))
 
 # sort all lists in decending order of numbeats
 sorted_idx = np.argsort(numbeats)[::-1]
@@ -84,18 +82,23 @@ dance_matrix1 = [dance_matrix1[i] for i in sorted_idx]
 dance_matrix2 = [dance_matrix2[i] for i in sorted_idx]
 dance_matrix3 = [dance_matrix3[i] for i in sorted_idx]
 dance_matrix4 = [dance_matrix4[i] for i in sorted_idx]
-dance_video1 = [dance_video1[i] for i in sorted_idx]
+dance_video10 = [dance_video10[i] for i in sorted_idx]
+dance_video11 = [dance_video11[i] for i in sorted_idx]
+dance_video12 = [dance_video12[i] for i in sorted_idx]
+dance_video13 = [dance_video13[i] for i in sorted_idx]
 dance_video2 = [dance_video2[i] for i in sorted_idx]
 dance_video3 = [dance_video3[i] for i in sorted_idx]
-dance_video4 = [dance_video4[i] for i in sorted_idx]
-correlations = [correlations[i] for i in sorted_idx]
+dance_video40 = [dance_video40[i] for i in sorted_idx]
+dance_video41 = [dance_video41[i] for i in sorted_idx]
+dance_video42 = [dance_video42[i] for i in sorted_idx]
+dance_video43 = [dance_video43[i] for i in sorted_idx]
 numbeats = [numbeats[i] for i in sorted_idx]
 
 css = "<head><link rel='stylesheet' href='mycss.css'></head>"
 html_code = css + "<center><table border=1><thead><tr><b><th>Sr. No.</th><th>Genre</th><th>Song Name</th><th>Audio</th><th>Duration (secs)</th><th>Number of beats</th><th>Music Matrix Downsize</th><th>Music Matrix Upsize</th><th>Dance Matrix Baseline 1 (un-synced, random actions from LRS)</th><th>Dance Video Baseline 1 (un-synced, random actions from LRS)</th><th>Dance Matrix Baseline 2 (un-synced, left to right)</th><th>Dance Video Baseline 2 (un-synced, left to right)</th><th>Dance Matrix Baseline 3 (synced, left to right)</th><th>Dance Video Baseline 3 (synced, left to right)</th><th>Dance Matrix Baseline 4 (synced, random actions from LRS)</th><th>Dance Video Baseline 4 (synced, random actions from LRS)</th></b></tr></thead><tbody>"
 
 for i in range(len(songs)):
-    row_data = "<td>%d.</td><td>%s</td><td>%s</td><td><audio controls><source src=%s type='audio/mpeg'></audio></td><td>%s</td><td>%d</td><td><img src=%s></td><td><img src=%s></td><td><figure><img src=%s><figcaption>Correlation-downsize = %f<br>Correlation-upsize = %f</figcaption></figure></td><td><video controls><source src=%s></video></td><td><figure><img src=%s><figcaption>Correlation-downsize = %f<br>Correlation-upsize = %f</figcaption></figure></td><td><video controls><source src=%s></video></td><td><figure><img src=%s><figcaption>Correlation-downsize = %f<br>Correlation-upsize = %f</figcaption></figure></td><td><video controls><source src=%s></video></td><td><figure><img src=%s><figcaption>Correlation-downsize = %f<br>Correlation-upsize = %f</figcaption></figure></td><td><video controls><source src=%s></video></td>" % (srno[i], genres[i], songnames[i], songs[i], durations[i], numbeats[i], music_matrix_downsize[i], music_matrix_upsize[i], dance_matrix1[i], correlations[i][0], correlations[i][4], dance_video1[i], dance_matrix2[i], correlations[i][1], correlations[i][5], dance_video2[i], dance_matrix3[i], correlations[i][2], correlations[i][6], dance_video3[i], dance_matrix4[i], correlations[i][3], correlations[i][7], dance_video4[i])
+    row_data = "<td>%d.</td><td>%s</td><td>%s</td><td><audio controls><source src=%s type='audio/mpeg'></audio></td><td>%s</td><td>%d</td><td><img src=%s></td><td><img src=%s></td><td><figure><img src=%s></figure></td><td><video controls><source src=%s></video><video controls><source src='%s'></video><br><video controls><source src='%s'></video><video controls><source src='%s'></video></td><td><figure><img src=%s></figure></td><td><video controls><source src=%s></video></td><td><figure><img src=%s></figure></td><td><video controls><source src=%s></video></td><td><figure><img src=%s></figure></td><td><video controls><source src=%s></video><video controls><source src=%s></video><br><video controls><source src=%s></video><video controls><source src=%s></video></td>" % (srno[i], genres[i], songnames[i], songs[i], durations[i], numbeats[i], music_matrix_downsize[i], music_matrix_upsize[i], dance_matrix1[i], dance_video10[i], dance_video11[i], dance_video12[i], dance_video13[i], dance_matrix2[i], dance_video2[i], dance_matrix3[i], dance_video3[i], dance_matrix4[i], dance_video40[i], dance_video41[i], dance_video42[i], dance_video43[i])
     html_code += "<tr>%s</tr>" % row_data
 
 html_code += "</tbody></table></center>"
